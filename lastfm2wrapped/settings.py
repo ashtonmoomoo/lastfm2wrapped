5ca88a54b764b5d1292bb2c0bd36bea67d8b54d9
Ashton Moore <ashton.chris.moore@gmail.com>
2021-08-29 14:58:42 +1200

    allow hosts

--- lastfm2wrapped/settings.py
+++ lastfm2wrapped/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lastfm2wrapped.herokuapp.com']
 
 
 # Application definition
